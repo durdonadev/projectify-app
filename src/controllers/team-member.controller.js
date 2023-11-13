7e89287c2181cbfce35fe440521073519b9408f4
Durdona Ibragimova <durdona.dev@gmail.com>
2023-11-13 00:15:11 -0500

    Ask email to set up password for team member

--- src/controllers/team-member.controller.js
+++ src/controllers/team-member.controller.js
@@ -26,16 +26,16 @@ class TeamMemberController {
     createPassword = catchAsync(async (req, res) => {
         const {
             query: { inviteToken },
-            body: { password, passwordConfirm }
+            body: { password, passwordConfirm, email }
         } = req;
 
         if (!inviteToken) {
             throw new CustomError("Invite Token is missing", 400);
         }
 
-        if (!password || !passwordConfirm) {
+        if (!password || !passwordConfirm || !email) {
             throw new CustomError(
-                "All fields are required: Password and Password Confirmation",
+                "All fields are required: Password and Password Confirmation, Email",
                 400
             );
         }
@@ -47,7 +47,7 @@ class TeamMemberController {
             );
         }
 
-        await teamMemberService.createPassword(inviteToken, password);
+        await teamMemberService.createPassword(inviteToken, password, email);
 
         res.status(200).json({
             message: "You successfully created a password. Now, you can log in"
--- src/services/team-member.service.js
+++ src/services/team-member.service.js
@@ -23,22 +23,23 @@ class TeamMemberService {
         );
     };
 
-    createPassword = async (inviteToken, password) => {
+    createPassword = async (inviteToken, password, email) => {
         const hashedInviteToken = crypto.hash(inviteToken);
         const hashedPassword = await bcrypt.hash(password);
 
-        const admin = await prisma.teamMember.findFirst({
+        const teamMember = await prisma.teamMember.findFirst({
             where: {
                 inviteToken: hashedInviteToken
             }
         });
 
-        if (!admin) {
+        if (!teamMember) {
             throw new CustomError("Invalid Token", 400);
         }
 
         await prisma.teamMember.update({
             where: {
+                email: email,
                 inviteToken: hashedInviteToken
             },
 
