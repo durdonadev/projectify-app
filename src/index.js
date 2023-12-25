3ffaf9d246f9aeb676089063907d40ebf5c39115
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-24 23:09:05 -0500

    Add cors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { GlobalError } from "./middlewares/global-error.middleware.js";
 import { projectRouter } from "./routes/project.routes.js";
 import { teamMemberRouter } from "./routes/team-member.routes.js";
 import { storyRouter } from "./routes/story.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
