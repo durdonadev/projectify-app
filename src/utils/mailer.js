9c48476239911909e53555e9e3b155da2b120279
Durdona Ibragimova <durdona.dev@gmail.com>
2024-04-04 12:47:35 -0400

    Account activation should navigate to frontend

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -11,10 +11,7 @@ class Mailer {
                 pass: process.env.MAILER_PASS
             }
         });
-        this.baseApiURL =
-            process.env.NODE_ENV === "local"
-                ? "http://localhost:4000"
-                : "https://projectify-app-api-express.onrender.com";
+
         this.baseUiURL = process.env.UI_BASE_URL;
     }
 
@@ -31,7 +28,7 @@ class Mailer {
             await this.send({
                 to: emailAddress,
                 subject: "Projectify App | Activate Your Account",
-                html: `<a href="${this.baseApiURL}/admins/activate?activationToken=${token}">Verify your email</a>`
+                html: `<a style="color: red;" href="${this.UI_BASE_URL}/admin/activate?activationToken=${token}">Verify your email</a>`
             });
         } catch (error) {
             throw error;
