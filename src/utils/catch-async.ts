ec5f2b4485c793e814d3cf31aab20fb2752ec8e6
Durdona Ibragimova <durdona.dev@gmail.com>
2024-06-11 01:15:49 -0400

    Convert catchAsync function to TypeScript

--- src/utils/catch-async.ts
+++ src/utils/catch-async.ts
@@ -1,13 +1,24 @@
-import { NextFunction } from "express";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as core from 'express-serve-static-core';
+import { Request, Response, NextFunction } from 'express';
 
-export const catchAsync = (
+export const catchAsync = <
+    P = core.ParamsDictionary,
+    ResBody = any,
+    ReqBody = any,
+    ReqQuery = core.Query,
+>(
     routeHandler: (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => Promise<void> | void
+        req: Request<P, ResBody, ReqBody, ReqQuery>,
+        res: Response<ResBody>,
+        next: NextFunction,
+    ) => Promise<void> | void,
 ) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
+    return async (
+        req: Request<P, ResBody, ReqBody, ReqQuery>,
+        res: Response<ResBody>,
+        next: NextFunction,
+    ) => {
         try {
             await routeHandler(req, res, next);
         } catch (error) {
